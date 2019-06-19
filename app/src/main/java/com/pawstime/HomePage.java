b5542268bc5e025f0e578ee330ea2241270ca7ab
Dustin Womack <mwomac12@gmail.com>
2019-06-18 22:42:00 -0400

    Add templates for mockups (#2)

---
+++ app/src/main/java/com/pawstime/HomePage.java
@@ -0,0 +1,34 @@
+package com.pawstime;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class HomePage extends BaseActivity {
+    ImageView homeLogo;
+    @Override
+    public int getContentViewId() {
+        return R.layout.home_page;
+    }
+
+    @Override
+    public int getNavigationMenuItemId() {
+        return R.id.navigation_home;
+    }
+
+    @Override
+    protected int getToolbarTitle() {
+        return R.string.toolbar_home;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        homeLogo = findViewById(R.id.homeLogo);
+
+        //Path information for a default picture
+        String imagePath = "drawable/ic_paws_time";
+        int imageKey = this.getApplicationContext().getResources().getIdentifier(imagePath, "drawable", "com.pawstime"); // Find the image key
+        homeLogo.setImageDrawable(this.getApplicationContext().getResources().getDrawable(imageKey)); // Set the image
+
+    }
+}
